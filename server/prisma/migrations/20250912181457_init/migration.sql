d42f23fa8850f00dbb4fe189dd1a35a1e19b1922
YogeshKisslay <yogeshkisslay004@gmail.com>
2025-09-12 23:45:27 +0530

    fix(deploy): Regenerate migrations for production compatibility

--- server/prisma/migrations/20250912181457_init/migration.sql
+++ server/prisma/migrations/20250912181457_init/migration.sql
@@ -4,10 +4,13 @@ CREATE TABLE `User` (
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `password` VARCHAR(191) NOT NULL,
+    `isVerified` BOOLEAN NOT NULL DEFAULT false,
+    `verificationToken` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_verificationToken_key`(`verificationToken`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
