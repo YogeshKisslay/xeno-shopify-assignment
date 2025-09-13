742feb13dcf04a0349841015f930ba17f1829e99
YogeshKisslay <yogeshkisslay004@gmail.com>
2025-09-13 11:01:19 +0530

    Fixed bug

--- server/src/routes/ingestionRoutes.js
+++ server/src/routes/ingestionRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { startInitialIngestion } = require('../controllers/ingestionController');
+
 const { authMiddleware } = require('../middleware/authMiddleware');
 
 router.post('/start', authMiddleware, startInitialIngestion);
